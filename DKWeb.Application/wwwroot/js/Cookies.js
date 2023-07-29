79a91ebf88687e7d6db5bba2c121695ccff23116
Dimistov <96862736+Dimistov@users.noreply.github.com>
2023-07-29 19:29:18 +0300

    Frontend

---
+++ DKWeb.Application/wwwroot/js/Cookies.js
@@ -0,0 +1,5 @@
+﻿// Cookie functions
+
+function setCookie(name, value) {
+    document.cookie = `${name}` + "=" + `${value}` + "; expires=Sun, 6 January 2030 12:00:00 UTC;";
+}
---
+++ DKWeb.Application/wwwroot/js/StickyNavbar.js
@@ -0,0 +1,22 @@
+﻿// STICKY NAVBAR
+
+// Onscroll event
+window.onscroll = function () { toggleStickinessFunction() };
+
+// Function to execute
+function toggleStickinessFunction() {
+    // Variables
+    var navbar = document.getElementById("appNavBar");
+
+    // Getting the highest point of the navbar
+    var navOffset = navbar.offsetTop;
+
+    // Instructing to add the sticky class to the navbar if the window offsetTop is higher than the navbar offset
+    if (window.pageYOffset > navOffset) {
+        navbar.classList.add("nav-sticky");
+        document.body.style.paddingTop = 100 + 'px';
+    } else {
+        navbar.classList.remove("nav-sticky");
+        document.body.style.paddingTop = 0 + 'px';
+    }
+}
